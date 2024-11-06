d8da5aa481438f7a07198f4c320a9cf5142d44b6
Hugo GALAN <hugo.galan8@gmail.com>
2024-11-06 11:55:27 +0000

    Fixing issue with .xpi

--- automatically-put-pgn.js
+++ automatically-put-pgn.js
@@ -1,9 +1,28 @@
-chrome.storage.local.get(['pgn'], function (result) {
-    if (result.pgn) {
-        const textarea = document.getElementById("pgn");
-        const button = document.getElementById("review-button");
-        textarea.value = result.pgn;
-        button.click();
-        chrome.storage.local.remove('pgn');
-    }
+window.addEventListener('load', () => {
+
+    chrome.storage.local.get(['pgn'], function (result) {
+        if (result.pgn) {
+            const textarea = document.getElementById("pgn");
+            const button = document.getElementById("review-button");
+            textarea.value = result.pgn;
+            // if button click throws an error, rety after 50ms 10 times and then give up
+            let retries = 10;
+            const clickButton = () => {
+                try {
+                    button.click();
+                } catch (e) {
+                    if (retries > 0) {
+                        retries--;
+                        setTimeout(clickButton, 50);
+                    }
+                }
+            };
+            clickButton();
+            chrome.storage.local.remove('pgn', () => {
+                if (chrome.runtime.lastError) {
+                    console.error("Error removing data:", chrome.runtime.lastError);
+                }
+            });
+        }
+    });
 });
--- override-report-button.js
+++ override-report-button.js
@@ -2,51 +2,48 @@ const config = {childList: true, subtree: true};
 
 
 function findButton(button) {
-    button.addEventListener('transitionend', function handleTransitionEnd(event) {
-        const oldButton = button;
-        const style = window.getComputedStyle(oldButton);
-        const newButton = document.createElement('button');
+    const oldButton = button;
+    const style = window.getComputedStyle(oldButton);
+    const newButton = document.createElement('button');
+    newButton.className = 'cc-button-component cc-button-primary cc-button-xx-large cc-button-full game-over-review-button-background';
 
-        for (var i = 0; i < style.length; i++) {
-            newButton.style[style[i]] = style.getPropertyValue(style[i]);
-        }
+    newButton.onclick = function () {
+        const modal = document.getElementById("share-modal");
+        const shareButton = document.querySelector('.share.live-game-buttons-button');
+        const oldDisplay = modal.style.display;
+        modal.style.display = 'none';
+        shareButton.click();
 
-        newButton.onclick = function () {
-            const modal = document.getElementById("share-modal");
-            const shareButton = document.querySelector('.share.live-game-buttons-button');
-            const oldDisplay = modal.style.display;
-            modal.style.display = 'none';
-            shareButton.click();
-
-            const modalObserver = new MutationObserver((mutationsList, observer) => {
-                for (const mutation of mutationsList) {
-                    if (mutation.type === 'childList') {
-                        const pgn_elem = document.querySelector('.share-menu-tab-pgn-textarea');
-                        if (!pgn_elem) {
-                            continue;
-                        }
-                        const modal_close = document.querySelector(".ui_modal-backdrop");
-                        const pgn = pgn_elem.value;
-                        modal_close.click();
-                        modal.style.display = oldDisplay;
-                        chrome.storage.local.set({pgn: pgn});
-
-                        window.open('https://chess.wintrcat.uk/', '_blank');
-
-                        modalObserver.disconnect();
-                        return;
+        const modalObserver = new MutationObserver((mutationsList, observer) => {
+            for (const mutation of mutationsList) {
+                if (mutation.type === 'childList') {
+                    const pgn_elem = document.querySelector('.share-menu-tab-pgn-textarea');
+                    if (!pgn_elem) {
+                        continue;
                     }
+                    const modal_close = document.querySelector(".ui_modal-backdrop");
+                    const pgn = pgn_elem.value;
+                    modal_close.click();
+                    modal.style.display = oldDisplay;
+                    chrome.storage.local.set({pgn: pgn}, () => {
+                        if (chrome.runtime.lastError) {
+                            console.error("Error setting data:", chrome.runtime.lastError);
+                            return;
+                        }
+                        window.open('https://chess.wintrcat.uk/', '_blank');
+                        modalObserver.disconnect();
+                    });
+                    return;
                 }
-            });
+            }
+        });
 
-            modalObserver.observe(modal, config);
+        modalObserver.observe(modal, config);
 
-        };
+    };
 
-        oldButton.parentNode && oldButton.parentNode.replaceChild(newButton, oldButton);
-        observer.disconnect();
-        button.removeEventListener('transitionend', handleTransitionEnd);
-    });
+    oldButton.parentNode && oldButton.parentNode.replaceChild(newButton, oldButton);
+    observer.disconnect();
 }
 
 
