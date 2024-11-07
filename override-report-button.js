bafaab1a88314605539331429550f48f1ca0b814
Hugo GALAN <hugo.galan8@gmail.com>
2024-11-07 01:29:42 +0000

    Fixing issue when after playing the button was not replace

--- override-report-button.js
+++ override-report-button.js
@@ -1,11 +1,28 @@
-const config = {childList: true, subtree: true};
+const config = { childList: true, subtree: true };
+const buttonTypes = {
+    'button.game-over-review-button-background:not(.custom-redirect)': 'cc-button-component cc-button-primary cc-button-xx-large cc-button-full game-over-review-button-background custom-redirect',
+    'button.game-review-buttons-button:not(.custom-redirect)': 'cc-button-component cc-button-primary cc-button-large cc-button-full game-review-buttons-button custom-redirect',
+};
 
+function throttle(func, limit) {
+    let inThrottle;
+    return function() {
+        const args = arguments;
+        const context = this;
+        if (!inThrottle) {
+            func.apply(context, args);
+            inThrottle = true;
+            setTimeout(() => inThrottle = false, limit);
+        }
+    };
+}
 
-function findButton(button) {
+function findButton(button, newClass) {
     const oldButton = button;
-    const style = window.getComputedStyle(oldButton);
     const newButton = document.createElement('button');
-    newButton.className = 'cc-button-component cc-button-primary cc-button-xx-large cc-button-full game-over-review-button-background';
+    newButton.className = newClass;
+    newButton.innerText = oldButton.innerText;
+    newButton.innerHTML = oldButton.innerHTML;
 
     newButton.onclick = function () {
         const modal = document.getElementById("share-modal");
@@ -15,45 +32,34 @@ function findButton(button) {
         shareButton.click();
 
         const modalObserver = new MutationObserver((mutationsList, observer) => {
-            for (const mutation of mutationsList) {
-                if (mutation.type === 'childList') {
-                    const pgn_elem = document.querySelector('.share-menu-tab-pgn-textarea');
-                    if (!pgn_elem) {
-                        continue;
-                    }
-                    const modal_close = document.querySelector(".ui_modal-backdrop");
-                    const pgn = pgn_elem.value;
-                    modal_close.click();
-                    modal.style.display = oldDisplay;
-                    chrome.storage.local.set({pgn: pgn}, () => {
-                        if (chrome.runtime.lastError) {
-                            console.error("Error setting data:", chrome.runtime.lastError);
-                            return;
-                        }
-                        window.open('https://chess.wintrcat.uk/', '_blank');
-                        modalObserver.disconnect();
-                    });
+            const pgn_elem = document.querySelector('.share-menu-tab-pgn-textarea');
+            if (!pgn_elem) return;
+            const modal_close = document.querySelector(".ui_modal-backdrop");
+            const pgn = pgn_elem.value;
+            modal_close.click();
+            modal.style.display = oldDisplay;
+            chrome.storage.local.set({ pgn: pgn }, () => {
+                if (chrome.runtime.lastError) {
+                    console.error("Error setting data:", chrome.runtime.lastError);
                     return;
                 }
-            }
+                window.open('https://chess.wintrcat.uk/', '_blank');
+                modalObserver.disconnect();
+            });
         });
 
         modalObserver.observe(modal, config);
-
     };
 
     oldButton.parentNode && oldButton.parentNode.replaceChild(newButton, oldButton);
-    observer.disconnect();
 }
 
-
-const observer = new MutationObserver((mutationsList, observer) => {
-    for (const mutation of mutationsList) {
-        if (mutation.type === 'childList') {
-            const button = mutation.target.querySelector('.game-over-review-button-background');
-            button && findButton(button);
+const observer = new MutationObserver(throttle((mutationsList, observer) => {
+    for (const buttonType in buttonTypes) {
+        const button = document.querySelector(buttonType);
+        if (button) {
+            findButton(button, buttonTypes[buttonType]);
         }
     }
-});
-
+}, 100));
 observer.observe(document.body, config);
